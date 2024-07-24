634531106dc5a03422d555f2291c33ea5179e566
Oluwaseyi Tommy <130176232+seshconcept@users.noreply.github.com>
2024-07-24 22:50:17 +0100

    fix linting issues

--- app/components/getStarted.tsx
+++ app/components/getStarted.tsx
@@ -1,28 +1,36 @@
 "use state";
-import React from 'react'
-import Image from 'next/image';
-import image from "../../public/assets/images/getstartedimg.png"
-import ButtonSave from './buttonSave';
+import React from "react";
+import Image from "next/image";
+import image from "../../public/assets/images/getstartedimg.png";
+import ButtonSave from "./buttonSave";
 
 const getStarted = () => {
   return (
     <div className="flex flex-col w-full h-[469px]">
-    <div className='flex flex-col items-center p-3'>
-        <div className='p-4'>
-        <Image src={image} alt="Get started image" 
-        className="w-[249px] h-[160px]"
-/>    </div>
+      <div className="flex flex-col items-center p-3">
+        <div className="p-4">
+          <Image
+            src={image}
+            alt="Get started image"
+            className="w-[249px] h-[160px]"
+          />{" "}
+        </div>
 
-<p className="text-2xl font-bold leading-6 ">Let's get you started</p>
-<br />
-<p className='text-sm'>Use the “Add new link” button to get started. Once you have more than one link, you can reorder and edit them. We’re here to help you share your profiles with everyone!</p>
+        <p className="text-2xl font-bold leading-6 ">
+          Let&apos;s get you started
+        </p>
+        <br />
+        <p className="text-sm">
+          Use the &lsquo;Add new link&lsquo; button to get started. Once you
+          have more than one link, you can reorder and edit them. We&apos;re
+          here to help you share your profiles with everyone!
+        </p>
+      </div>
 
-</div>
+      <hr className="border-1 border-gray-200" />
+      <ButtonSave />
+    </div>
+  );
+};
 
-<hr className='border-1 border-gray-200'/>
-<ButtonSave />
-</div>
-  )
-}
-
-export default getStarted
+export default getStarted;
